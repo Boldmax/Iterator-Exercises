778f6bf532fcfd1a2839adf8642935309350af59
Boldmax <maxbold0007@gmail.com>
2020-08-27 17:26:06 +0100

    Iterator Exercises

---
+++ script.js
@@ -0,0 +1,97 @@
+//  Part 1
+
+var users = [
+    {
+      username: 'larry',
+      email: 'larry@foo.com',
+      yearsExperience: 22.1,
+      favoriteLanguages: ['Perl', 'Java', 'C++'],
+      favoriteEditor: 'Vim',
+      hobbies: ['Fishing', 'Sailing', 'Hiking'],
+      hometown: {
+        city: 'San Francisco',
+        state: 'CA'
+      }
+    },
+    {
+      username: 'jane',
+      email: 'jane@test.com',
+      yearsExperience: 33.9,
+      favoriteLanguages: ['Haskell', 'Clojure', 'PHP'],
+      favoriteEditor: 'Emacs',
+      hobbies: ['Swimming', 'Biking', 'Hiking'],
+      hometown: {
+        city: 'New York',
+        state: 'NY'
+      }
+    },
+    {
+      username: 'sam',
+      email: 'sam@test.com',
+      yearsExperience: 8.2,
+      favoriteLanguages: ['JavaScript', 'Ruby', 'Python', 'Go'],
+      favoriteEditor: 'Atom',
+      hobbies: ['Golf', 'Cooking', 'Archery'],
+      hometown: {
+        city: 'Fargo',
+        state: 'SD'
+      }
+    },
+    {
+      username: 'anne',
+      email: 'anne@test.com',
+      yearsExperience: 4,
+      favoriteLanguages: ['C#', 'C++', 'F#'],
+      favoriteEditor: 'Visual Studio Code',
+      hobbies: ['Tennis', 'Biking', 'Archery'],
+      hometown: {
+        city: 'Albany',
+        state: 'NY'
+      }
+    },
+    {
+      username: 'david',
+      email: 'david@test.com',
+      yearsExperience: 12.5,
+      favoriteLanguages: ['JavaScript', 'C#', 'Swift'],
+      favoriteEditor: 'VS Code',
+      hobbies: ['Volunteering', 'Biking', 'Coding'],
+      hometown: {
+        city: 'Los Angeles',
+        state: 'CA'
+      }
+    }
+  ];
+
+  
+  // Write a function called printEmails which console.log's each email for the users.
+
+  function printEmails(){
+     users.forEach(function(value){
+       console.log(value.email)
+      });
+  }
+
+//  Write a function called printHobbies which console.log's each hobby for each user.
+
+ function printHobbies(){
+ users.forEach(function(value){
+    console.log(value.hobbies)
+    });
+}
+
+/*  Write a function called findHometownByState which returns the first user which has 
+a hometown of the state that is passed in*/
+function findHometownByState(home){
+  return users.filter(function(val){
+    return (val.state == home)
+  });
+}
+
+// Write a function called allLanguages which returns an array of all of the unique values
+
+function allLanguages(){
+ return users.map(function(val){
+    return val.favoriteLanguages
+  })
+}
